28a19a10f8a703b2c8c01aef7379884200f84efe
Silvan Kohler <silvan@famkohler.com>
2023-06-19 18:18:48 +0200

    fix: do not show unformatted text when loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
 'use client';
-import { NextUIProvider } from '@nextui-org/react';
+import { Loading, NextUIProvider } from '@nextui-org/react';
 import GCMSection from './components/gcmsection';
 import Header from './components/header';
+import { useState, useEffect } from 'react';
+import { Flex } from './components/styles/flex';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
   return (
-    <NextUIProvider>
-      <main>
-        <Header />
-        <GCMSection />
-      </main>
-    </NextUIProvider>
+      isLoading ? (
+        <main />
+      ) : (
+        <NextUIProvider>
+          <main>
+            <Header />
+            <GCMSection />
+          </main>
+        </NextUIProvider>
+      )
   );
 }
