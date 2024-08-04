5e2fd380c9b2a18e28e11cce437a375939072b74
Silvan Kohler <silvan@silvankohler.swiss>
2024-08-04 18:24:13 +0200

    feat: initialize shadcn with darkmode
    Signed-off-by: Silvan Kohler <silvan@silvankohler.swiss>

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,58 @@
+import clsx from "clsx";
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+  PreventFlashOnWrongTheme,
+  ThemeProvider,
+  useTheme,
+} from "remix-themes";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+import { themeSessionResolver } from "./sessions.server";
+import "./tailwind.css";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import {
+  useLoaderData,
+  Meta,
+  Links,
+  Outlet,
+  ScrollRestoration,
+  Scripts,
+  LiveReload,
+} from "@remix-run/react";
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme(),
+  };
+}
+// Wrap your app with ThemeProvider.
+// `specifiedTheme` is the stored theme in the session storage.
+// `themeAction` is the action name that's used to change the theme in the session storage.
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <html lang="en">
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  );
+}
+
+export function App() {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
+  return (
+    <html lang="en" className={clsx(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
---
+++ app/sessions.server.tsx
@@ -0,0 +1,21 @@
+import { createCookieSessionStorage } from "@remix-run/node";
+import { createThemeSessionResolver } from "remix-themes";
+
+// You can default to 'development' if process.env.NODE_ENV is not set
+const isProduction = process.env.NODE_ENV === "production";
+
+const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "theme",
+    path: "/",
+    httpOnly: true,
+    sameSite: "lax",
+    secrets: ["not-so-secret-secret"],
+    // Set domain and secure only if in production
+    ...(isProduction
+      ? { domain: "health.silvankohler.swiss", secure: true }
+      : {}),
+  },
+});
+
+export const themeSessionResolver = createThemeSessionResolver(sessionStorage);
